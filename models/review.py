a14949fc42cb2cca487b0959b2f96ff5fd084046
Ebenezer Ogidiolu <ogidioluebenezer@gmail.com>
2023-03-22 19:04:48 -0700

    models/user.py

--- models/review.py
+++ models/review.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
-"""Defines the Review class."""
-from models.base_model import BaseModel
+""" Review module for the HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+from os import getenv
 
 
-class Review(BaseModel):
-    """Represent a review.
-
-    Attributes:
-        place_id (str): The Place id.
-        user_id (str): The User id.
-        text (str): The text of the review.
+class Review(BaseModel, Base):
+    """ The review class
     """
 
-    place_id = ""
-    user_id = ""
-    text = ""
+    __tablename__ = 'reviews'
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
