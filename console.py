dc6256b282bf7c588a78ea7cc1a51485338f0b7d
Ebenezer Ogidiolu <ogidioluebeenzer@gmail.com>
2023-05-19 01:56:54 +0000

    modifications

--- console.py
+++ console.py
@@ -127,13 +127,13 @@ class HBNBCommand(cmd.Cmd):
                     arg_splited[1] = arg_splited[1].replace("_", " ")
                     arg_splited[1] = arg_splited[1].replace('"', '\\"')
                 kw[arg_splited[0]] = arg_splited[1]
+            new_instance = HBNBCommand.classes[arg_list[0]](**kw)
+            new_instance.save()
+            print(new_instance.id)
         except SyntaxError:
             print("** class name missing **")
         except NameError:
             print("** class doesn't exist **")
-        new_instance = HBNBCommand.classes[arg_list[0]](**kw)
-        new_instance.save()
-        print(new_instance.id)
 
     def help_create(self):
         """ Help information for the create method """
@@ -330,3 +330,4 @@ class HBNBCommand(cmd.Cmd):
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
+
